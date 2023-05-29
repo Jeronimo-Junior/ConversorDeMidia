b06b576873932f269ae91e3239d5b2a8cb781afc
Felipe-Alves-VNGX <fsa032003@gmail.com>
2023-05-29 10:58:48 -0300

    link não funcionando

--- sd-video-converter/video/video-conversion.js
+++ sd-video-converter/video/video-conversion.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const path = require('path');
 const { exec } = require('child_process');
 const Minio = require('minio');
+const { URL } = require('url');
+
 
 const kafkaHost = 'kafka:9092';
 const kafkaTopic = 'video-topic';
@@ -21,9 +23,9 @@ const consumer = new Consumer(
 const minioClient = new Minio.Client({
   endPoint: 'minio',
   port: 9000,
-  useSSL: false,
+  useSSL: true,
   accessKey: 'root',
-  secretKey: 'minio_password'
+  secretKey: '12345678'
 });
 
 // Configuração do Nodemailer
@@ -55,68 +57,90 @@ consumer.on('message', async (message) => {
   }
 
   // Salva o arquivo de entrada no diretório correspondente
-  const inputFile = path.join(inputDir, file.originalname);
+  const inputFile = path.join(inputDir, filename);
   try {
-    const fileData = minioClient.getObject('input-bucket',filename);
-    fs.writeFileSync(inputFile, fileData);
-    console.log('Arquivo salvo:', inputFile);
+    minioClient.fGetObject('input-bucket', filename, inputFile, (error) => {
+      if (error) {
+        console.error('Erro ao salvar o arquivo:', error);
+        return;
+      }
+      console.log('Arquivo salvo:', inputFile);
+
+      // Define o caminho e o nome do arquivo de saída convertido
+      const outputFilename = `${filename.split('.').shift()}.${format}`;
+      const outputFile = path.join(outputDir, outputFilename);
+
+      // Comando para realizar a conversão do vídeo usando o FFMpeg (é necessário tê-lo instalado)
+      const command = `ffmpeg -i "${inputFile}" "${outputFile}"`;
+
+      // Executa o comando para converter o vídeo
+
+      exec(command, (error) => {
+        if (error) {
+          console.log('Erro ao converter o vídeo:', error.message);
+        } else {
+          console.log('Vídeo convertido com sucesso:', outputFile);
+
+          //
+          const outputBucket = 'output-bucket';
+          const objectName = outputFile;
+
+          minioClient.presignedPutObject(outputBucket, objectName, (err, presignedUrl) => {
+            if (err) {
+              console.log('Error uploading file to MinIO:', error);
+              sendEmail(email, 'Error uploading converted file', 'An error occurred while uploading the converted file. Please try again later.');
+            } else {
+                // Replace the host and port in the presigned URL
+                const urlObj = new URL(presignedUrl);
+                urlObj.host = 'localhost:9000';
+                const updatedPresignedUrl = urlObj.toString();
+
+                console.log('Presigned URL:', updatedPresignedUrl);
+                sendEmail(email, 'File converted and uploaded', `Your file has been converted and uploaded. Download it using the following link (valid for 24 hours): ${updatedPresignedUrl}`);
+                  }
+          })
+          // minioClient.fPutObject(outputBucket, objectName, outputFile, (error) => {
+          //   // Remove the input file from the input bucket
+          //   //fs.unlinkSync(inputFile);
+          //   if (error) {
+          //     console.log('Error uploading file to MinIO:', error);
+          //     sendEmail(email, 'Error uploading converted file', 'An error occurred while uploading the converted file. Please try again later.');
+          //   } else {
+          //     console.log('File uploaded to MinIO:', objectName);
+
+          //     // Generate a temporary download URL for the file
+          //     const expiryTime = 24 * 60 * 60; // 24 hours in seconds
+          //     minioClient.presignedGetObject(outputBucket, objectName, expiryTime, (error, presignedUrl) => {
+          //       fs.unlinkSync(outputFile);
+          //       if (error) {
+          //         console.log('Error generating presigned URL:', error);
+          //         sendEmail(email, 'Error generating download link', 'An error occurred while generating the download link for the converted file. Please try again later.');
+          //       } else {
+          //         // Replace the host and port in the presigned URL
+          //         const urlObj = new URL(presignedUrl);
+          //         urlObj.host = 'localhost:9000';
+          //         const updatedPresignedUrl = urlObj.toString();
+
+          //         console.log('Presigned URL:', updatedPresignedUrl);
+          //         sendEmail(email, 'File converted and uploaded', `Your file has been converted and uploaded. Download it using the following link (valid for 24 hours): ${updatedPresignedUrl}`);
+          //       }
+          //     });
+
+          //   }
+          // });
+        }
+      });
+    });
   } catch (error) {
     console.error('Erro ao salvar o arquivo:', error);
     return;
   }
 
-  
-  // Define o caminho e o nome do arquivo de saída convertido
-  const outputFilename = `${file.originalname.split('.').shift()}.${format}`;
-  const outputFile = path.join(outputDir, outputFilename);
 
- 
-
-  // Comando para realizar a conversão do vídeo usando o FFMpeg (é necessário tê-lo instalado)
-  const command = `ffmpeg -i "${inputFile}" "${outputFile}"`;
-
-  // Executa o comando para converter o vídeo
-  
-  exec(command, (error) => {
-    if (error) {
-      console.log('Erro ao converter o vídeo:', error.message);
-    } else {
-      console.log('Vídeo convertido com sucesso:', outputFile);
-    }
-  });
-
-  //
-  const outputBucket = 'output-bucket';
-  const objectName = outputFilename;
-  const fileStream = fs.createReadStream(outputFile);
-
-  minioClient.putObject(outputBucket, objectName, fileStream, (error) => {
-    // Remove the input file from the input bucket
-    fs.unlinkSync(inputFile);
-
-    if (error) {
-      console.log('Error uploading file to MinIO:', error);
-      sendEmail(email, 'Error uploading converted file', 'An error occurred while uploading the converted file. Please try again later.');
-    } else {
-      console.log('File uploaded to MinIO:', objectName);
-
-      // Generate a temporary download URL for the file
-      const expiryTime = 24 * 60 * 60; // 24 hours in seconds
-      minioClient.presignedGetObject(outputBucket, objectName, expiryTime, (error, presignedUrl) => {
-        if (error) {
-          console.log('Error generating presigned URL:', error);
-          sendEmail(email, 'Error generating download link', 'An error occurred while generating the download link for the converted file. Please try again later.');
-        } else {
-          console.log('Presigned URL:', presignedUrl);
-          sendEmail(email, 'File converted and uploaded', `Your file has been converted and uploaded. Download it using the following link (valid for 24 hours): ${presignedUrl}`);
-        }
-      });
-    }
-  });
 });
 
 // Função para enviar e-mails
-function sendEmail(to, subject, body,outputFile) {
+function sendEmail(to, subject, body, outputFile) {
   const mailOptions = {
     from: 'videoconversor5@gmail.com',
     to: to,
@@ -129,7 +153,6 @@ function sendEmail(to, subject, body,outputFile) {
       console.error('Erro ao enviar e-mail:', error);
     } else {
       console.log('E-mail enviado:', info.response);
-      fs.unlinkSync(outputFile);
     }
   });
 }
